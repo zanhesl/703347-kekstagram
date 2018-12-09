356beac71cf6ddedd31d6c4622a549428d57527f
Keks <keks@htmlacademy.ru>
2018-12-09 23:25:46 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -15,6 +15,23 @@ var usersComments = [
 
 var usersName = ['Акакий', 'Зигмунд', 'Апполинарий', 'Дормидонт', 'Сигизмунд', 'Феофан'];
 
+// Создаём объект с данными для комментариев
+var comments = [];
+
+var getComments = function (count) {
+
+  for (var i = 0; i < count; i++) {
+    var comment = {
+      avatar: 'img/avatar-' + getRandomInt(1, 6) + '.svg',
+      message: usersComments[getRandomInt(0, 5)],
+      name: usersName[getRandomInt(0, 5)]
+    };
+  }
+  comments.push(comment);
+  return comments;
+};
+
+
 // Создаем массив объектов с даннми описывающими фото
 
 var cardsData = [];
@@ -23,7 +40,9 @@ var getObjects = function (quantity) {
   for (var i = 1; i < quantity + 1; i++) {
     var object = {
       url: 'photos/' + i + '.jpg',
-      like: getRandomInt(15, 200)
+      like: getRandomInt(15, 200),
+      comments: getComments(2),
+      description: 'Тестим новую камеру!'
     };
 
     cardsData.push(object);
@@ -33,67 +52,72 @@ var getObjects = function (quantity) {
 
 getObjects(25);
 
-// Создаем элементы в разметке
+// Создаем элементы для маленьких фото в разметке
 
 var pictureListElement = document.querySelector('.pictures');
 
-var createPictureElement = function () {
+var createPictureElement = function (number) {
 
   var fragment = document.createDocumentFragment();
   var picture = document.querySelector('#picture')
     .content
     .querySelector('.picture');
 
-  for (var i = 0; i < 25; i++) {
+  for (var i = 0; i < number; i++) {
 
     var picturesItems = document.createElement('div');
 
     var pictureElement = picture.cloneNode(true);
     pictureElement.querySelector('.picture__img').src = cardsData[i].url;
     pictureElement.querySelector('.picture__likes').textContent = cardsData[i].like;
-    pictureElement.querySelector('.picture__comments').textContent = cardsData[i].message;
+    pictureElement.querySelector('.picture__comments').textContent = cardsData[i].comments[i].message;
 
     picturesItems.appendChild(pictureElement);
     fragment.appendChild(picturesItems);
   }
-
   pictureListElement.appendChild(fragment);
-
 };
 
-createPictureElement();
+createPictureElement(25);
 
-// Создаём объект с данными для комментариев
+// Создаём элементы для комментариев
 
-var comments = [];
+var commentListItems = document.querySelector('.social__comments');
 
-var getComments = function (count) {
-  for (var i = 0; i < count; i++) {
-    var comment = {
-      avatar: 'img/avatar-' + getRandomInt(1, 6) + '.svg',
-      message: usersComments[getRandomInt(0, 5)],
-      name: usersName[getRandomInt(0, 5)],
-      commentsCount: getRandomInt(1, 200)
-    };
+var createCommentElement = function (number) {
+
+  var fragment = document.createDocumentFragment();
+  var liForComment = document.querySelector('.social__comment');
+
+
+  for (var i = 0; i < number; i++) {
+    var commentElement = liForComment.cloneNode(true);
+    commentElement.querySelector('.social__picture').src = cardsData[0].comments[i].avatar;
+    commentElement.querySelector('.social__text').textContent = cardsData[0].comments[i].message;
+
+    fragment.appendChild(commentElement);
   }
-  comments.push(comment);
+  commentListItems.appendChild(fragment);
 };
 
-getComments(2);
+createCommentElement(getRandomInt(0, 5));
 
-// Показываем большое фото и вставляем комментарии в разметку
+// Показываем большое фото
 
-var getBigPictureComments = function (count) {
+var getBigPicture = function () {
 
   var userPicture = document.querySelector('.big-picture');
   userPicture.classList.remove('hidden');
   userPicture.querySelector('img').src = cardsData[0].url;
   userPicture.querySelector('.likes-count').textContent = cardsData[0].like;
-  userPicture.querySelector('.comments-count').textContent = comments[0].commentsCount;
-  for (var i = 0; i < count; i++) {
-    userPicture.querySelector('.social__comments .social__picture').src = comments[i].avatar;
-    userPicture.querySelector('.social__comments .social__text').textContent = comments[i].message;
-  }
+  userPicture.querySelector('.comments-count').textContent = cardsData[0].comments.length;
+  userPicture.querySelector('.social__caption').textContent = cardsData[0].description;
 };
 
-getBigPictureComments(2);
+getBigPicture();
+
+var commentCount = document.querySelector('.social__comment-count');
+var commentLoad = document.querySelector('.comments-loader');
+
+commentCount.classList.add('visually-hidden');
+commentLoad.classList.add('visually-hidden');
